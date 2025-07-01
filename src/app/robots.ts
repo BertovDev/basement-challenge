fe1682d7ff30f7557bccfeab01ca5348bb2087fb
BertovDev <bertobautista18@gmail.com>
2025-07-01 18:52:48 -0300

    Change domain

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
       disallow: "/private/",
     },
-    sitemap: "https://basement-challenge-nu.vercel.app/sitemap.xml",
+    sitemap: "https://basement-challenge-berto-bautista.vercel.app/sitemap.xml",
   };
 }
