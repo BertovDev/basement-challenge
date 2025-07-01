71c693cc02514eda635d630db6c2f34de58765e3
BertovDev <bertobautista18@gmail.com>
2025-07-01 13:54:59 -0300

    Add loder init anim

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Loader() {
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="flex items-center justify-center h-screen loader ">
       <div className="animate-pulse">
         <Image
           src="/logo_mobile.svg"
