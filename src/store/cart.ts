58100348904ce1ab1e5f98ca336fbbc150ff08f6
BertovDev <bertobautista18@gmail.com>
2025-07-02 09:33:15 -0300

    Fix lint

--- src/store/cart.ts
+++ src/store/cart.ts
@@ -1,7 +1,6 @@
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
 import { CartItemType, Product } from "../types";
-import { useRef } from "react";
 
 type CartState = {
   items: CartItemType[];
